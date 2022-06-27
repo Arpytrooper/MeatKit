d7a86d586420d91f9547662d27cde63437111317
Arpytrooper <90275734+Arpytrooper@users.noreply.github.com>
2022-06-27 12:24:55 -0400

    Revert "Revert "Revert "Allow the detection and use of different plugin classes per build profile"""
    This reverts commit 6031e7390b0e1d0349cacb9baa06dc4c75694d1f.

--- Assets/MeatKit/Editor/BuildPipeline/MeatKitBuildException.cs
+++ Assets/MeatKit/Editor/BuildPipeline/MeatKitBuildException.cs
@@ -4,12 +4,9 @@ namespace MeatKit
 {
     public class MeatKitBuildException : Exception
     {
-        public MeatKitBuildException(string message) : base(message)
-        {
-        }
-
         public MeatKitBuildException(string message, Exception innerException) : base(message, innerException)
         {
+            
         }
     }
 }
